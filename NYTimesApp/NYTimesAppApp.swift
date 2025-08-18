5c7aca0e3caada12b4e81e3a7e4e168ab77d47cd
Muhammad Murtaza <Muhammadmurtaza10@gmail.com>
2025-08-18 23:31:31 +0500

    Initial Commit

---
+++ NYTimesApp/NYTimesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesAppApp.swift
+//  NYTimesApp
+//
+//  Created by Murtaza on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
